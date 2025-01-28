b850f62b8ecac207b7259c0a9f4bf76f0a600d84
smrutidatap <smruti.sahoo@dataprowess.com.au>
2025-01-28 12:50:52 +0530

    updated config

--- bugback/settings.py
+++ bugback/settings.py
@@ -146,11 +146,11 @@ WSGI_APPLICATION = "bugback.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.environ("DB_NAME"),
+        "USER": os.environ("DB_USER"),
+        "PASSWORD": os.environ("DB_PASSWORD"),
+        "HOST": os.environ("DB_HOST"),
+        "PORT": os.environ("DB_PORT"),
     }
 }
 
